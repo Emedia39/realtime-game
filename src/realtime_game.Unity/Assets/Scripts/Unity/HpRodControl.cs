8f55963687c2c581881130b91fdeee1b9da3cb93
Emedia39 <241207@st.yoshida-g.ac.jp>
2025-12-17 15:27:34 +0900

    2025/12/17 ・プレイヤータグが照準の中央に来た時、射撃命令(デバッグログログのみ) ・HPバーの実装※ダメージを受ける手段…現時点ではない ・武器を構えると照準が出て、下げると消える ※！照準時ズームするのは1つ前のプッシュからある

---
+++ src/realtime_game.Unity/Assets/Scripts/Unity/HpRodControl.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPUI : MonoBehaviour
+{
+    public int maxHP = 100;
+    public int currentHP = 100;
+
+    public Image hpBarImage; // HPバー
+    public Text Hp;          // HP数値
+
+    //public Color blueColor = Color.cyan;//new Color(37f, 182f, 252f, 255f);
+    //public Color yellowColor = Color.yellow;//new Color(241f, 187f, 46f, 255f);
+    //public Color redColor = Color.red;// new Color(255f, 79f, 65f, 255f);
+
+    public Color blueColor = new Color(37f, 182f, 252f, 255f);//= Color.cyan;
+    public Color yellowColor = new Color(241f, 187f, 46f, 255f);//= Color.yellow;
+    public Color redColor = new Color(255f, 79f, 65f, 255f);// = Color.red;
+
+    private RectTransform barRT;
+    private float maxBarWidth;
+
+    void Start()
+    {
+        barRT = hpBarImage.GetComponent<RectTransform>();
+        maxBarWidth = barRT.rect.width;
+        UpdateHPUI();
+    }
+
+    public void SetHP(int hp)
+    {
+        currentHP = Mathf.Clamp(hp, 0, maxHP);
+        UpdateHPUI();
+    }
+
+    void UpdateHPUI()
+    {
+        // テキスト
+        Hp.text = currentHP.ToString();
+
+        // バー幅
+        float rate = (float)currentHP / maxHP;
+        barRT.SetSizeWithCurrentAnchors(
+            RectTransform.Axis.Horizontal,
+            maxBarWidth * rate
+        );
+
+        // 色切り替え
+        if (currentHP > 50)
+            hpBarImage.color = blueColor;
+        else if (currentHP > 20)
+            hpBarImage.color = yellowColor;
+        else
+            hpBarImage.color = redColor;
+    }
+
+}
--- src/realtime_game.Unity/Assets/Scripts/Unity/PlayerControl.cs
+++ src/realtime_game.Unity/Assets/Scripts/Unity/PlayerControl.cs
@@ -1,17 +1,24 @@
 using System;
 using System.Threading;
+using Unity.Burst.Intrinsics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 //using static UnityEditor.PlayerSettings;
 
 public class PlayerControl : MonoBehaviour
 {
+    //スクリプト
+    [SerializeField] WeaponControl weaponControl;
+
+    //[SerializeField] private float radiusPixel = 20f; // レティクル許容半径（px）
+    /*// カメラ内の「特定範囲」例：中央60%だけ判定
+    [Range(0f, 1f)] public float minX = 0.4999f;//0.2
+    [Range(0f, 1f)] public float maxX = 0.5111f;//0.8
+    [Range(0f, 1f)] public float minY = 0.4999f;//0.2
+    [Range(0f, 1f)] public float maxY = 0.5111f;//0.8*/
+
     //三種の武器(配列)
     [SerializeField] private GameObject[] weapons; //0:P320,1:EP8,2:M4
-    /*[SerializeField] private GameObject WeaponsAll;//GameObject型の変数を宣言※ゲームオブジェクトをアタッチ
-    [SerializeField] private GameObject WeaponM4;
-    [SerializeField] private GameObject WeaponEP8;
-    [SerializeField] private GameObject WeaponP320;*/
 
     public static int WeaponsNom;//武器の識別番号
 
@@ -25,6 +32,9 @@ public class PlayerControl : MonoBehaviour
     private float moveSpeed = 5.0f;//プレイヤー移動速度
     private float jumpPower = 5.0f;//ジャンプ上昇速度
     private bool isJump = false;//
+
+    //画像
+    [SerializeField] private GameObject aim; //赤い照準(AIM)
 
     // FPS視点パラメータ
     public Transform neck;                  // プレイヤーの首のTransformを指定
@@ -62,6 +72,8 @@ public class PlayerControl : MonoBehaviour
         int index = WeaponsNom;
         if (index >= 0 && index < weapons.Length)
             weapons[index].SetActive(true);
+
+        //武器が○○だったらリーチやダメージ変化とかやりたい
     }
 
     // Update is called once per frame
@@ -162,6 +174,16 @@ public class PlayerControl : MonoBehaviour
         if (isWeaponHold == true)//クリックしていたら
         {
             WeaponHold();//武器を構える
+
+            if (weaponControl.IsPlayerInReticle())
+            {
+                //Debug.Log("プレイヤー(タグ)がカメラの特定範囲内");
+                if (weaponControl.CurrentDistance > 0)
+                {
+                    Debug.Log("射撃！");
+                }
+                
+            }
         }
         else
         {
@@ -214,6 +236,7 @@ public class PlayerControl : MonoBehaviour
 
     public void WeaponHold()//武器を構える時の処理
     {
+        aim.SetActive(true);//赤い照準を出す
         cam.fieldOfView = 20.0f;//有効視野(FOV)を小さく(ズームイン)
         weapons[0].transform.localPosition = new Vector3(0.0f, +0.043f, 0.40f);//親+位置/※左右上下前後
         weapons[1].transform.localPosition = new Vector3(0.0f, -0.170f, 0.40f);//親+位置/※左右上下前後
@@ -222,6 +245,7 @@ public class PlayerControl : MonoBehaviour
     }
     public void WeaponDown()//武器を下ろす時の処理
     {
+        aim.SetActive(false);//赤い照準を消す
         cam.fieldOfView = 60.0f;//有効視野(FOV)を戻す(ズームアウト)
         weapons[0].transform.localPosition = new Vector3(0.4f, 0.0f, 0.6f);//親+位置/※左右上下前後
         weapons[1].transform.localPosition = new Vector3(0.4f, 0.0f, 0.6f);//親+位置/※左右上下前後
