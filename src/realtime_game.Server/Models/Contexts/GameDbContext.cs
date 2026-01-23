0f670bea1f445ce2358f04d173aaa4c8c9b86b43
Emedia39 <241207@st.yoshida-g.ac.jp>
2026-01-24 07:49:37 +0900

    99_satoshi_vmの導入

--- src/realtime_game.Server/Models/Contexts/GameDbContext.cs
+++ src/realtime_game.Server/Models/Contexts/GameDbContext.cs
@@ -8,13 +8,18 @@ namespace realtime_game.Server.Models.Contexts
         public DbSet<User> Users { get; set; }
         //テーブル追加(エンティティ追加)したらここに追加していく
 
-        readonly string connectionString =
-"server=localhost;database=realtime_game;user=jobi;password=jobi;";
+#if DEBUG
+        readonly string connectionString = "server=localhost;port=3306;database=realtime_game;user=jobi;password=jobi;";//realtime_game
+# else
+        readonly string connectionString = "server=db-ge0202400.mysql.database.azure.com;port=3306;database=realtime_game241207;user=student;password=Yoshidajobi2024;SslMode=Required;";
+# endif
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseMySql(connectionString,
 new MySqlServerVersion(new Version(8, 0)));
         }
+
     }
 
 }
